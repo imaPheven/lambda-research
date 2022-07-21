653bdb5f3db0845424edae18147cdedf6a67f09d
Steven Jansick <ima@codenalldaylong.com>
2022-07-20 22:41:28 -0500

    Storybook / next checkpoint

---
+++ storybook/src/stories/pages/about.stories.jsx
@@ -0,0 +1,10 @@
+// /stories/pages/about.stories.jsx
+
+import About from "../../pages/about";
+
+export default {
+  title: "Pages/About",
+  component: About,
+};
+
+export const AboutPage = () => <About />
